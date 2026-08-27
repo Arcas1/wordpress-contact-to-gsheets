cfb4d67676c5f20f6c3134a41478fa0f9ebc890d
William Daza <william.daza.66@gmail.com>
2026-08-27 10:04:07 -0500

    chore: scaffold plugin, Composer deps, and PHPUnit tooling
    Trim google/apiclient-services to the Sheets service only to keep vendor/ small.

---
+++ tests/TestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace C2GS\Tests;
+
+use Brain\Monkey;
+use Mockery;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase {
+
+	protected function setUp(): void {
+		parent::setUp();
+		Monkey\setUp();
+	}
+
+	protected function tearDown(): void {
+		Monkey\tearDown();
+		Mockery::close();
+		parent::tearDown();
+	}
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+require dirname( __DIR__ ) . '/vendor/autoload.php';
+
+define( 'MINUTE_IN_SECONDS', 60 );
+define( 'HOUR_IN_SECONDS', 3600 );
+define( 'DAY_IN_SECONDS', 86400 );
+define( 'WEEK_IN_SECONDS', 604800 );
